d482ad23976240f27682e6c7bc7987a4cf57ea91
sudhanshuverma <sudhanshu@ballisticlearning.com>
2011-09-27 22:00:06 +0530

    block my progress initial commit

---
+++ myprogress/block_myprogress.php
@@ -0,0 +1,106 @@
+<?php
+
+require_once($CFG->libdir.'/completionlib.php');
+
+class block_myprogress extends block_list {
+    function init() {
+        $this->title = get_string('pluginname', 'block_myprogress');
+    }
+
+    function get_content() {
+        global $CFG, $DB, $OUTPUT,$COURSE,$course;       
+
+        if($this->content !== NULL) {
+            return $this->content;
+        }
+
+        $this->content = new stdClass;
+        $this->content->items = array();
+        $this->content->icons = array();
+        $this->content->footer = '';
+        $completedactivities=0;
+        $incompletedactivities=0;
+        $savedactivities=0;
+        $notattemptedactivities=0;
+        $waitingforgradeactivities=0;
+        $savedactivities=0;
+
+        $course = $this->page->course;
+        require_once($CFG->dirroot.'/course/lib.php');
+
+        $modinfo = get_fast_modinfo($course);
+        $modfullnames = array();
+        $completion = new completion_info($course);
+        $activities = $completion->get_activities();
+        if($completion->is_enabled()){
+        
+        foreach($activities as $activity){
+            $data = $completion->get_data($activity, true, $userid=0, null);
+            $completionstate=$data->completionstate;
+            if($completionstate==-1){
+                $waitingforgradeactivities++;
+                
+            }
+            
+            elseif($completionstate==0){
+                $notattemptedactivities++;
+                
+            }
+ 
+            elseif($completionstate==1 || $completionstate==2){
+                $completedactivities++;
+                
+            }
+
+            elseif($completionstate==3){
+                $incompletedactivities++;
+            }
+            else{
+                $savedactivities++;
+            }           
+        
+        }                 
+        $context = get_context_instance(CONTEXT_COURSE, $course->id);
+        
+        if(!has_capability('moodle/course:update',$context) && !is_guest($context)&&
+                !has_capability('moodle/grade:viewall',$context) && !is_guest($context)){
+             $this->content->items[] =  '<a href="' . $CFG->wwwroot . '/blocks/myprogress/listactivities.php?id=' . $course->id . '&show=completed' .
+                        '&navlevel=top">' . $completedactivities . ' Completed</a>';
+             $this->content->icons[] = '<img src="' . $CFG->wwwroot . '/blocks/myprogress/pix/completed.gif"
+                                                    height="16" width="16" alt="">'; 
+             $this->content->items[] = '<a href="' . $CFG->wwwroot . '/blocks/myprogress/listactivities.php?id=' . $course->id . '&show=incompleted' .
+                        '&navlevel=top">' . $incompletedactivities . ' Incomplete</a>';
+             $this->content->icons[] = '<img src="' . $CFG->wwwroot . '/blocks/myprogress/pix/incomplete.gif"
+                                                    height="16" width="16" alt="">';
+             $this->content->items[] = '<a href="' . $CFG->wwwroot . '/blocks/myprogress/listactivities.php?id=' . $course->id . '&show=saved' .
+                        '&navlevel=top">' . $savedactivities . ' Saved</a>';
+             $this->content->icons[] = '<img src="' . $CFG->wwwroot . '/blocks/myprogress/pix/saved.gif"
+                                                    height="16" width="16" alt="">';
+             $this->content->items[] = '<a href="' . $CFG->wwwroot . '/blocks/myprogress/listactivities.php?id=' . $course->id . '&show=notattempted' .
+                        '&navlevel=top">' . $notattemptedactivities . ' Not Attempted</a>';
+             $this->content->icons[] = '<img src="' . $CFG->wwwroot . '/blocks/myprogress/pix/notattempted.gif"
+                                                    height="16" width="16" alt="">';
+             $this->content->items[] = '<a href="' . $CFG->wwwroot . '/blocks/myprogress/listactivities.php?id=' . $course->id . '&show=waitingforgrade' .
+                        '&navlevel=top">'. $waitingforgradeactivities . ' Waiting for grade</a>';
+             $this->content->icons[] = '<img src="' . $CFG->wwwroot . '/blocks/myprogress/pix/unmarked.gif"
+                                                    height="16" width="16" alt="">'; 
+             return $this->content;
+        }
+        else{
+             $this->content->items[] = "My progress block content is only shown to student";
+             $this->content->icons[] = "";
+        }
+        
+    
+    }
+    else{
+        $this->content->items[] = "Activity tracking is not enabled";
+        $this->content->icons[] = "";
+    }
+        
+    }
+
+    function applicable_formats() {
+        return array('all' => false,'course-*' => true);
+    }
+}
---
+++ myprogress/listactivities.php
@@ -0,0 +1,224 @@
+<?php
+
+require_once('../../config.php');
+require_once($CFG->dirroot . '/course/lib.php');
+require_once('lib.php');
+require_once($CFG->libdir . '/completionlib.php');
+global $CFG, $DB, $OUTPUT, $PAGE, $COURSE;
+
+$USINGHTMLEDITOR = false;
+
+$id = required_param('id', PARAM_INT);      // course id
+$show = optional_param('show', 'notloggedin', PARAM_ALPHA);
+
+//  Paging options:
+$page = optional_param('page', 0, PARAM_INT);
+$perpage = optional_param('perpage', 2, PARAM_INT);
+$PAGE->set_url('/blocks/myprogress/listactivities.php', array('id' => $id, 'show' => $show, 'navlevel' => 'top'));
+
+if (!$course = $DB->get_record("course", array("id" => $id))) {
+    print_error("Course ID was incorrect");
+}
+
+require_login($course);
+$context = get_context_instance(CONTEXT_COURSE, $course->id);
+$istudent = has_capability('mod/assignment:submit', $context);
+
+// only teachers should see this!
+if (!$istudent) {
+    print_error("Only students can use this page!");
+}
+
+require_login($course->id);  /// Sets up $COURSE and therefore the theme.
+$completedactivities = 0;
+$incompletedactivities = 0;
+$savedactivities = 0;
+$notattemptedactivities = 0;
+$waitingforgradeactivities = 0;
+$savedactivities = 0;
+
+$completion = new completion_info($course);
+$activities = $completion->get_activities();
+
+if ($completion->is_enabled()) {
+
+    foreach ($activities as $activity) {
+        $data = $completion->get_data($activity, true, $userid = 0, null);
+        $activitystate = $data->completionstate;
+        if ($activitystate == -1) {
+            $waitingforgradeactivities++;
+        } elseif ($activitystate == 0) {
+            $notattemptedactivities++;
+        } elseif ($activitystate == 1 || $activitystate == 2) {
+            $completedactivities++;
+        } elseif ($activitystate == 3) {
+            $incompletedactivities++;
+        } else {
+            $savedactivities++;
+        }
+    }
+}
+
+switch ($show) {
+
+    case 'completed':
+
+        $activities_results = $completedactivities;
+        $name = get_string('breadcrumb:activitiescompleted', 'block_myprogress');
+        $title = get_string('title:completed', 'block_myprogress') . " (Total:" . $completedactivities . " Activities)";
+        break;
+
+    case 'incompleted':
+
+        $activities_results = $incompletedactivities;
+        $name = get_string('breadcrumb:activitiesincompleted', 'block_myprogress');
+        $title = get_string('title:incompleted', 'block_myprogress') . " (Total:" . $incompletedactivities . " Activities)";
+        break;
+
+    case 'saved':
+
+        $activities_results = $savedactivities;        
+        $name = get_string('breadcrumb:saved', 'block_myprogress');
+        $title = get_string('title:saved', 'block_myprogress') . " (Total:" . $savedactivities . " Activities)";
+        break;
+
+    case 'notattempted':
+        $activities_results = $notattemptedactivities;
+        $name = get_string('breadcrumb:notattempted', 'block_myprogress');
+        $title = get_string('title:notattempted', 'block_myprogress') . " (Total:" . $notattemptedactivities . " Activities)";
+        break;
+
+    case 'waitingforgrade':
+        $activities_results = $waitingforgradeactivities;
+        $name = get_string('breadcrumb:waitingforgrade', 'block_myprogress');
+        $title = get_string('title:waitingforgrade', 'block_myprogress') . " (Total:" . $waitingforgradeactivities . " Activities)";
+        break;
+    default:
+        break;
+}
+
+/// Print header
+$navlinks = array(array('name' => $name, 'link' => '', 'type' => 'misc'));
+$navigation = build_navigation($navlinks);
+$heading = $course->fullname;
+print_header_simple($title, $heading, $navigation, '', '', true, '', '');
+
+echo "<div id='mark-interface'>";
+echo "<h4 class='head-title' style='padding-bottom:12px;'>$title</h4>\n";
+
+// use paging
+$totalcount = $activities_results;
+//$baseurl = 'listactivities.php?id=' . $id . '&show=' . $show . '&navlevel=top';
+//$pagingbar = new paging_bar($totalcount, $page, $perpage, $baseurl, 'page');
+//echo $OUTPUT->render($pagingbar);
+
+echo '<table width="96%" class="markingcontainerList" border="0" cellpadding="0" cellspacing="0" align="center">' . '<tr><td class="intd">';
+
+echo '<table  width="100%" border="0" cellpadding="0" cellspacing="0">';
+if (($show == 'completed' || $show == 'incompleted' || $show == 'saved' || $show == 'notattempted' || $show == 'waitingforgrade') && $totalcount >0 && $activities>0) {
+    echo "<tr>";
+    echo "<th align='center' width='15%'><strong>Activity type </strong></th>";
+    echo "<th align='left' width='67%' style='text-align:left;'><strong>Activity or Resource Name </strong></th>";
+    echo "<th align='center' width='18%'><strong>Section <strong></th>";
+    echo "</tr>";
+}
+else{
+    echo '<div style="text-align:center; padding:12px;">';
+    echo "No activity with status ". get_string($show, 'block_myprogress')."";
+    echo "</div>";
+}
+// iterate
+//for ($i = ($page * $perpage); ($i < ($page * $perpage) + $perpage) && ($i < $totalcount); $i++) {
+
+if ($show == 'completed') {
+    if($activities){
+        foreach ($activities as $activity) {
+            $data = $completion->get_data($activity, false, $userid = 0, null);
+
+            $activitystate = $data->completionstate;
+            if ($activitystate == 1 || $activitystate == 2) {
+                echo "<tr><td align='center'>\n";
+                $modtype = $DB->get_field('modules', 'name', array('id' => $activity->module));
+                $modicon = "<IMG BORDER=0 VALIGN=absmiddle SRC=\"$CFG->wwwroot/mod/$modtype/pix/icon.gif\" HEIGHT=\"16\" WIDTH=\"16\" >";            
+                echo $modtype;
+                echo "</td>\n";
+                echo "<td align='left'><a href='" . $CFG->wwwroot . "/mod/$modtype/view.php?id=$data->coursemoduleid'>$modicon</a><a href='" . $CFG->wwwroot . "/mod/$modtype/view.php?id=$data->coursemoduleid' style=\"padding-left:4px\">" . $activity->name . "</a></td>\n";
+                echo "<td align='center'>Section-$activity->section</td></tr>\n";
+            }
+        }
+    }
+} else if ($show == 'incompleted') {
+    if($activities){
+        foreach ($activities as $activity) {
+            $data = $completion->get_data($activity, true, $userid = 0, null);
+            $activitystate = $data->completionstate;
+            if ($activitystate == 3) {
+                echo "<tr><td align='center'>\n";
+                $modtype = $DB->get_field('modules', 'name', array('id' => $activity->module));
+                $modicon = "<IMG BORDER=0 VALIGN=absmiddle SRC=\"$CFG->wwwroot/mod/$modtype/pix/icon.gif\" HEIGHT=\"16\" WIDTH=\"16\" >";            
+                echo $modtype;
+                echo "</td>\n";
+                echo "<td align='left'><a href='" . $CFG->wwwroot . "/mod/$modtype/view.php?id=$data->coursemoduleid'>$modicon</a><a href='" . $CFG->wwwroot . "/mod/$modtype/view.php?id=$data->coursemoduleid' style=\"padding-left:4px\">" . $activity->name . "</a></td>\n";
+                echo "<td align='center'>Section-$activity->section</td></tr>\n";
+            }
+        }    
+    }
+} else if ($show == 'notattempted') {
+    if($activities){
+        foreach ($activities as $activity) {
+            $data = $completion->get_data($activity, true, $userid = 0, null);
+            $activitystate = $data->completionstate;
+            if ($activitystate == 0) {
+                echo "<tr><td align='center'>\n";
+                $modtype = $DB->get_field('modules', 'name', array('id' => $activity->module));
+                $modicon = "<IMG BORDER=0 VALIGN=absmiddle SRC=\"$CFG->wwwroot/mod/$modtype/pix/icon.gif\" HEIGHT=\"16\" WIDTH=\"16\" >";            
+                echo $modtype;
+                echo "</td>\n";
+                echo "<td align='left'><a href='" . $CFG->wwwroot . "/mod/$modtype/view.php?id=$data->coursemoduleid'>$modicon</a><a href='" . $CFG->wwwroot . "/mod/$modtype/view.php?id=$data->coursemoduleid' style=\"padding-left:4px\">" . $activity->name . "</a></td>\n";
+                echo "<td align='center'>Section-$activity->section</td></tr>\n";
+            }
+        }
+    }
+} else if ($show == 'waitingforgrade') {
+    if($activities){
+        foreach ($activities as $activity) {
+            $data = $completion->get_data($activity, true, $userid = 0, null);
+            $activitystate = $data->completionstate;
+            if ($activitystate == -1) {
+                echo "<tr><td align='center'>\n";
+                $modtype = $DB->get_field('modules', 'name', array('id' => $activity->module));
+                $modicon = "<IMG BORDER=0 VALIGN=absmiddle SRC=\"$CFG->wwwroot/mod/$modtype/pix/icon.gif\" HEIGHT=\"16\" WIDTH=\"16\" >";            
+                echo $modtype;
+                echo "</td>\n";
+                echo "<td align='left'><a href='" . $CFG->wwwroot . "/mod/$modtype/view.php?id=$data->coursemoduleid'>$modicon</a><a href='" . $CFG->wwwroot . "/mod/$modtype/view.php?id=$data->coursemoduleid' style=\"padding-left:4px\">" . $activity->name . "</a></td>\n";
+                echo "<td align='center'>Section-$activity->section</td></tr>\n";
+            }
+        }
+  }
+} else if ($show == 'saved') {
+    if($activities){
+        foreach ($activities as $activity) {
+            $data = $completion->get_data($activity, true, $userid = 0, null);
+            $activitystate = $data->completionstate;
+            if ($activitystate == 4) {
+                echo "<tr><td align='center'>\n";
+                $modtype = $DB->get_field('modules', 'name', array('id' => $activity->module));
+                echo $modtype;
+                $modicon = "<IMG BORDER=0 VALIGN=absmiddle SRC=\"$CFG->wwwroot/mod/$modtype/pix/icon.gif\" HEIGHT=\"16\" WIDTH=\"16\" >";            
+                echo $modtype;
+                echo "</td>\n";
+                echo "<td align='left'><a href='" . $CFG->wwwroot . "/mod/$modtype/view.php?id=$data->coursemoduleid'>$modicon</a><a href='" . $CFG->wwwroot . "/mod/$modtype/view.php?id=$data->coursemoduleid' style=\"padding-left:4px\">" . $activity->name . "</a></td>\n";
+                echo "<td align='center'>Section-$activity->section</td></tr>\n";
+            }
+        }
+    }
+} else {
+    
+}
+//}
+echo"</table>\n";
+
+echo '</td></tr></table>';
+
+echo "</div>";
+echo $OUTPUT->footer($course);
