330b598006689ac0cad82af7c0352844528d16b4
Michael Gardener <mgardener@cissq.com>
2013-03-11 01:21:04 -0300

    Update lib.php

--- lib.php
+++ lib.php
@@ -9,7 +9,7 @@
  * @todo Finish documenting this function
  */ 
 
-function assignment_status($mod, $userid) {
+function assignment_status($mod, $userid, $resubmission = false) {
     global $CFG, $DB, $USER, $SESSION;
     require_once ($CFG->dirroot.'/mod/assignment/lib.php');    
 
@@ -75,24 +75,63 @@ function assignment_status($mod, $userid) {
         if  (!($assignment = $DB->get_record('assign', array('id' => $mod->instance)))) {
             return false; // Doesn't exist
         }
-        if (!$submission = $DB->get_record('assign_submission', array('assignment'=>$assignment->id, 'userid'=>$USER->id))) {
-            return false;
-        }
-
-        //$submissionisgraded = $DB->record_exists('assign_grades', array('assignment'=>$assignment->id, 'userid'=>$USER->id));
-        if (!$submissionisgraded = $DB->get_record('assign_grades', array('assignment'=>$assignment->id, 'userid'=>$USER->id))) {
-            $graded = false;
-        }else if ($submissionisgraded->grade <> ''){
-            $graded = true;  
+        if ($resubmission){
+            if (!$submission = $DB->get_records('assign_submission', array('assignment'=>$assignment->id, 'userid'=>$USER->id), 'submissionnum DESC', '*', 0, 1)) {
+                return false;
+            }else{
+                $submission = reset($submission);
+            }
         }else{
-            $graded = false;
-        }        
-
-        if ($submission->status == 'draft') {
-            return 'saved';
-        } else if ($submission->status == 'submitted' && !$graded) {
-            return 'submitted';
+            if (!$submission = $DB->get_record('assign_submission', array('assignment'=>$assignment->id, 'userid'=>$USER->id))) {
+                return false;
+            }
         }
+
+        if ($resubmission){
+            if ($submissionisgraded = $DB->get_records('assign_grades', array('assignment'=>$assignment->id, 'userid'=>$USER->id, 'submissionnum' => $submission->submissionnum), 'submissionnum DESC', '*', 0, 1)) {
+                $submissionisgraded = reset($submissionisgraded);
+                if ($submissionisgraded->grade > -1){
+                    $graded = true;  
+                }else{
+                    $graded = false;
+                }                
+            }else {
+                $graded = false;
+            } 
+        }else{
+            if ($submissionisgraded = $DB->get_record('assign_grades', array('assignment'=>$assignment->id, 'userid'=>$USER->id))) {
+                if ($submissionisgraded->grade > -1){
+                    $graded = true;  
+                }else{
+                    $graded = false;
+                }                 
+            }else {
+                $graded = false;
+            } 
+        }       
+
+        
+        if ($submission->status == 'draft') {
+            if($graded){
+                return 'submitted';
+            }else{
+                return 'saved';
+            }            
+        }
+        if ($submission->status == 'resub') {
+            if($graded){
+                return 'submitted';
+            }else{
+                return 'waitinggrade';
+            }            
+        } 
+        if ($submission->status == 'submitted') {
+            if($graded){
+                return 'submitted';
+            }else{
+                return 'waitinggrade';
+            }  
+        }        
     } else {
         return false;
     }
